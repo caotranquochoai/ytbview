a4f03ab29e22ecd89087fd9757fdcf530f50b919
caotranquochoai <54810821+caotranquochoai@users.noreply.github.com>
2020-07-26 20:15:12 +0700

    Update star.bat

--- star.bat
+++ star.bat
@@ -1,6 +1,5 @@
 @echo off
 start "khoi dong YTB client sau 8s" ./YTBClient.exe start
 ping 127.0.0.1 -n 8 > NUL
---------------------------
 start "khoi dong YTB View sau 10s" ./YTBVIEWER.exe start 
 ping 127.0.0.1 -n 15 > NUL
